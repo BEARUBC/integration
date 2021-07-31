61c849829c9981348d411b5e25861de66e42b460
Nathan <nathannpoon@gmail.com>
2021-07-31 15:23:33 -0600

    moving basic test here

--- integration/src/lib.rs
+++ integration/src/lib.rs
@@ -1,7 +1,13 @@
+pub mod pythonCaller;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    fn calling() {
+        super::pythonCaller::test();
     }
 }
